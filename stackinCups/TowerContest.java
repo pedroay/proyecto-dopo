ca63dd34a6368dca8527f9ae8222b83b78b88cd5
javiercaicezo <javier.caicezo@gmail.com>
2026-03-19 22:10:46 -0500

    Agrego solve

--- stackinCups/TowerContest.java
+++ stackinCups/TowerContest.java
@@ -7,21 +7,37 @@
  */
 public class TowerContest
 {
-    public void solve(int n, int h){
+    public String solve(int n, int h){
         int nh = h * 10;
         int maxAltura = n*n*10;
-        if(nh > maxAltura){
-            return ;
+        if(nh > maxAltura || h<=0){
+            return "Imposible";
         }
         Tower torre = new Tower(n);
-        int actual =torre.height();
-        for(int i = n;i>= 1;i--){
-            int altura = torre.findCupByNumber(i).getHeight();
-            int reduccion = altura - 20;
-            if(actual - reduccion >= nh){
-                actual = actual - reduccion;
-                //aqui toca buscar el que es inmediatamente menor para meterlo dentro 
+        while (torre.height() > nh) {
+            String[][] par = torre.swapToReduce();
+            if (par.length == 0) {
+                return "Imposible";
             }
+            torre.swap(par[0], par[1]);
         }
+        
+        String[][] items = torre.stakingItems();
+        String result = "";
+        for (int i = 0; i < items.length; i++) {
+            int num=Integer.parseInt(items[i][1]);
+            int altura=num*2-1;
+            result += " " + altura + "\n";
+        }
+        result = result.trim();
+        System.out.println(result);
+        //torre.makeVisible();
+        return result;
+    }
+    public void simulate(int n, int h){
+        String result=solve(n,h);
+        if(result=="Imposible"){ 
+        }
+    
     }
 }
