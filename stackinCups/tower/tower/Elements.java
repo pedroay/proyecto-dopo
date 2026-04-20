2f99ba63c3db1485484b12bbe7c5a1cbbf02a5ea
unknown <ayalapedro5e@gmail.com>
2026-04-20 12:46:00 -0500

    cambie reglas de diseño de cup

--- stackinCups/tower/tower/Elements.java
+++ stackinCups/tower/tower/Elements.java
@@ -1,5 +1,6 @@
 package tower;
 import java.util.Random;
+import java.util.List;
 import java.util.ArrayList;
 
  /*+ Write a description of class elements here.
@@ -9,27 +10,109 @@ import java.util.ArrayList;
  */
 public abstract class Elements
 {
+	/**
+     * The width of the element in pixels.
+     */
     protected int width;
+
+    /**
+     * The color representation of the element.
+     */
     protected String color;
+
+    /**
+     * The horizontal position (X-coordinate) of the element on the canvas.
+     */
     protected int posx;
+
+    /**
+     * The vertical position (Y-coordinate) of the element on the canvas.
+     */
     protected int posy;
+
+    /**
+     * Indicates whether the element is currently visible on the screen.
+     */
     protected boolean isVisible;
+
+    /**
+     * The identification number or size index of the element.
+     */
     protected int number;
+
+    /**
+     * The vertical dimension (height) of the element.
+     */
     protected int height;
+
+    /**
+     * The specific type or category of the element.
+     */
     protected String type;
+
+    /**
+     * Reference to the element positioned directly above this one.
+     */
     protected Elements above;
+
+    /**
+     * Determines if another element can be placed inside this one.
+     */
     protected boolean canIn;
+
+    /**
+     * Reference to the tower instance that contains this element.
+     */
     protected Tower torre;
+
+    /**
+     * Indicates if the element can be removed from its current position.
+     */
     protected boolean isQuitable;
+
+    /**
+     * Specifies if this element has the ability to remove lids.
+     */
     protected boolean eliminaTapas;
+
+    /**
+     * Specifies if this element can displace other elements in the stack.
+     */
     protected boolean desplazaElementos;
+
+    /**
+     * Flag to identify if the element behaves according to "Crazy" logic.
+     */
     protected boolean isCrazy;
+
+    /**
+     * Flag to identify if the element behaves according to "Fearful" logic.
+     */
     protected boolean isFearful;
-    protected ArrayList<Integer> notQuitablePosition;
+
+    /**
+     * List of positions where the element cannot be removed.
+     */
+    protected List<Integer> notQuitablePosition;
+
+    /**
+     * Indicates if the element is classified as a Box.
+     */
     protected boolean isBox;
+
+    /**
+     * Indicates if this element is currently contained inside a box.
+     */
     protected boolean isInABox;
     
-    public Elements(int number){
+    /**
+     * Initializes a new Element with default properties based on its size number.
+     * Calculates the initial width and sets coordinates, visibility, 
+     * and behavioral flags to their default starting values.
+     * * @param number The identification number used to calculate 
+     * the width and define the element's size.
+     */
+    public Elements(final int number){
         width = calculateWidth(number);
         posx = 150 -(width/2);
         posy = 0;
@@ -40,16 +123,38 @@ public abstract class Elements
         isCrazy = false;
         isFearful = false;
         isQuitable = true;
-        notQuitablePosition = new ArrayList<Integer>();
+        notQuitablePosition = new ArrayList<>();
         isBox = false;
         isInABox = false;
     }
     
-    public final int calculateWidth(int inumber){
+    /**
+     * Calculates the width of an element based on its 
+     * specific numeric identifier.
+     * The formula uses the identifier to determine 
+     * a proportional size in pixels.
+     *
+     * @param inumber The numeric identifier used 
+     * as the base for the calculation.
+     * @return The calculated width of the element 
+     * in pixels.
+     */
+    public final int calculateWidth(final int inumber){
         return ((2*inumber)-1)*10;
     }
     
+    /**
+     * Draws the element on the canvas.
+     * This method must be implemented by subclasses to define 
+     * the specific graphical representation of the object.
+     */
     public abstract void draw();
+    
+    /**
+     * Erases the element from the canvas.
+     * This method must be implemented by subclasses to 
+     * remove or hide their graphical representation.
+     */
     public abstract void erase();
     public  int getNumber(){
         return number;
@@ -64,17 +169,24 @@ public abstract class Elements
     }
     
     /**
-     * retorna un color apartir de una lista 
+     * Returns a random color string from a predefined list.
+     * @return A string representing a color (e.g., "red", "blue").
      */
     protected String randomColor(){
-        String[] colors = {"red","black","blue","yellow","green","magenta"};
-        Random random = new Random();
+        final String[] colors = {"red","black","blue","yellow","green","magenta"};
+        final Random random = new Random();
 
-        int index = random.nextInt(colors.length);
+        final int index = random.nextInt(colors.length);
         return colors[index];
     }
     
-    protected void changeColor(String ncolor){
+    /**
+     * Updates the current color of the element with 
+     * a new specified color string.
+     * * @param ncolor The new color to be applied 
+     * to the element.
+     */
+    protected void changeColor(final String ncolor){
         color=ncolor;
     }
     
@@ -90,30 +202,46 @@ public abstract class Elements
         return height;
     }
     
-    public  void setPosx(int newPosx) {
+    
+    public  void setPosx(final int newPosx) {
         posx = newPosx;
     }
 
-    public void setPosy(int newPosy) {
+    public void setPosy(final int newPosy) {
         posy = newPosy;
     }
     
-    public void setPosition(int newPosx, int newPosy) {
+    /**Updates the position of the element to the 
+     * specified coordinates on the canvas.
+     * @param newPosx The new horizontal coordinate 
+      * (X position) for the element.
+      * @param newPosy The new vertical coordinate 
+      * (Y position) for the element.
+     */
+    public void setPosition(final int newPosx, final int newPosy) {
         posx = newPosx;
         posy = newPosy;
     }
     
+    /**
+     * Changes the visibility of the element to true 
+     * and renders it on the canvas.
+     */
     public void makeVisible() {
         isVisible = true;
         draw();
     }
     
+    /**
+     * Changes the visibility of the element to false 
+     * and removes it from the canvas.
+     */
     public void makeInvisible() {
         isVisible = false;
         erase();
     }
     
-    public boolean isVisible() {
+    public boolean hasBeenVisible() {
         return isVisible;
     }
     
@@ -191,7 +319,7 @@ public abstract class Elements
 
     public abstract void push(int i);
 
-    public boolean isQuitable() {
+    public boolean thisIsQuitable() {
         return isQuitable;
     }
 
@@ -199,7 +327,7 @@ public abstract class Elements
         isQuitable = value;
     }
 
-    public boolean isCrazy() {
+    public boolean thisIsCrazy() {
         return isCrazy;
     }
 
@@ -207,7 +335,7 @@ public abstract class Elements
         isCrazy = value;
     }
 
-    public boolean isFearful() {
+    public boolean thisIsFearful() {
         return isFearful;
     }
 
@@ -223,14 +351,14 @@ public abstract class Elements
         return notQuitablePosition.contains(i);
     }
 
-    public ArrayList<Integer> getNotQuitablePosition(){
+    public List<Integer> getNotQuitablePosition(){
         return notQuitablePosition;
     }
     public final void setIsBox(boolean value) {
         isBox = value;
     }
 
-    public boolean isBox() {
+    public boolean thisIsBox() {
         return isBox;
     }
 
@@ -238,7 +366,7 @@ public abstract class Elements
         isInABox = value;
     }
 
-    public boolean isInABox() {
+    public boolean thisIsInABox() {
         return isInABox;
     }
     public void createHisLid(){}
